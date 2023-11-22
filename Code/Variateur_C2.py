9f130ec17bff1c12cdbb6ba4d9dbb6e1e342c206
YaTchance <tchanceya@outlook.fr>
2023-11-22 21:43:53 +0100

    Fonctionnement +

--- Code/Variateur_C2.py
+++ Code/Variateur_C2.py
@@ -5,6 +5,7 @@ import HandTrackingModule as htm
 import math
 ##########################################
 wCam, hCam = 1280, 720 #Résolution de l'écran
+print(f"Résolution de la de l'écran : {wCam}x{hCam}")
 ##########################################
 
 detector = htm.handDetector(detectionCon=0)
@@ -13,7 +14,8 @@ pTime = 0
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
- ########################################################################################################################    
+
+########################################################################################################################    
 # Function Start
 while True:
     success, img = cap.read()
@@ -32,32 +34,30 @@ while True:
 ########################################################################################################################            
         length = math.hypot(x2-x1, y2-y1)
         print(length)
-        
-            # Gradation de couleur
-        color = (0, 255, 0)  # Couleur par défaut (vert)
-        if length <= 40:
-            color = (0, 255, 0)  # Cercle en vert
-        else:
-            # Interpolation linéaire pour obtenir la couleur en fonction de la longueur
-            color = (
-                int(255 * (length - 40) / (800 - 40)), 255 - int(255 * (length - 40) / (400 - 40)),0,
-            )
+        # Normalisation de la valeur de length à la plage 0-255
+        length = int(255 * (length - 40) / (1000 - 40))
+        length = max(0, min(255, length))  # Assurer que la valeur est entre 0 et 255
+
+        # Interpolation linéaire entre vert (0, 255, 0) et bleu(255, 0, 0)
+        color = (int((1 - length / 255) * 255), int((length / 255) * 255), 0)
 
         cv2.circle(img, (cx, cy), 10, color, cv2.FILLED)
-
-        # Afficher la graduation de couleur
-        cv2.rectangle(img, (50, int(length)), (85, 400), color, cv2.FILLED)
+        #Afficher la barre graduer
+    cv2.rectangle(img, (50, hCam - int(length)), (100, hCam), color, cv2.FILLED)
+    cv2.putText(img, f'{int(length)}', (50, hCam - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 ########################################################################################################################        
   
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
 ########################################################################################################################        
+   
+    flipped = cv2.flip(img, 2)
     
-    flipped = cv2.flip(img, 1)
     cv2.putText(flipped, f'FPS: {int(fps)}', (40,50), cv2.FONT_HERSHEY_COMPLEX,
               1, (255,0,100),2) #Changement de taille d'écriture FPS
     cv2.imshow("image", flipped)    #Inverser le sens de la vidéo
+    
 ########################################################################################################################    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
