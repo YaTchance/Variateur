267debebea0fdc66eee5811bbb13a4f4cc0d34e9
YaTchance <tchanceya@outlook.fr>
2023-11-24 18:15:16 +0100

    Avec un passe try

--- Code/Variateur_C2.py
+++ Code/Variateur_C2.py
@@ -22,29 +22,39 @@ while True:
     img = detector.findHands(img)
     lmlist = detector.findPosition(img, draw=False)
 ########################################################################################################################    
-    if (len(lmlist)):
+    if len(lmlist):
         x1, y1 = lmlist[4][1], lmlist[4][2]
         x2, y2 = lmlist[8][1], lmlist[8][2]
-        cx, cy = (x1+x2) // 2, (y1+y2)//2
-        
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+           
+        length = math.hypot(x2 - x1, y2 - y1)
+      
         cv2.circle(img, (x1,y1), 10, (255,0,255), cv2.FILLED)
         cv2.circle(img, (x2,y2), 10, (255,0,255), cv2.FILLED)
         cv2.line (img, (x1,y1),(x2,y2), (255,0,0),2 )
         cv2.circle(img, (cx,cy),10, (255,0,255), cv2.FILLED)
 ########################################################################################################################            
-        length = math.hypot(x2-x1, y2-y1)
-        print(length)
+      
         # Normalisation de la valeur de length à la plage 0-255
         length = int(255 * (length - 40) / (1000 - 40))
         length = max(0, min(255, length))  # Assurer que la valeur est entre 0 et 255
-
-        # Interpolation linéaire entre vert (0, 255, 0) et bleu(255, 0, 0)
-        color = (int((1 - length / 255) * 255), int((length / 255) * 255), 0)
-
-        cv2.circle(img, (cx, cy), 10, color, cv2.FILLED)
-        #Afficher la barre graduer
-    cv2.rectangle(img, (50, hCam - int(length)), (100, hCam), color, cv2.FILLED)
-    cv2.putText(img, f'{int(length)}', (50, hCam - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+        print(length)
+ # Calculer les composantes de couleur en fonction de la longueur
+    try: 
+        color_red_to_blue = (int((1 - length / 255) * 255), 0, int((length / 255) * 255))
+        color_blue_to_green = (0, int((1 - length / 255) * 255), int((length / 255) * 255))
+    
+# Combinaison des deux couleurs pour obtenir une variation de couleur complète
+        color = (
+        color_red_to_blue[0] + color_blue_to_green[0],
+        color_red_to_blue[1] + color_blue_to_green[1],
+        color_red_to_blue[2] + color_blue_to_green[2]
+            )
+   
+        cv2.rectangle(img, (50, hCam - int(length)), (100, hCam), color, cv2.FILLED)
+    except: 
+            pass
 ########################################################################################################################        
   
     cTime = time.time()
@@ -53,6 +63,9 @@ while True:
 ########################################################################################################################        
    
     flipped = cv2.flip(img, 2)
+    # Affichage de la barre de graduation avec une couleur variable
+
+    cv2.putText(flipped, f'{int(length)}', (50, hCam - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
     
     cv2.putText(flipped, f'FPS: {int(fps)}', (40,50), cv2.FONT_HERSHEY_COMPLEX,
               1, (255,0,100),2) #Changement de taille d'écriture FPS
