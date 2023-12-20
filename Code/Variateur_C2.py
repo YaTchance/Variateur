5a472c78137d341833ab38473dce42ec71d1fa88
YaTchance <tchanceya@outlook.fr>
2023-12-20 15:45:30 +0100

    Point de couleur

--- Code/Variateur_C2.py
+++ Code/Variateur_C2.py
@@ -23,55 +23,64 @@ while True:
     lmlist = detector.findPosition(img, draw=False)
 ########################################################################################################################    
     if len(lmlist):
-        x1, y1 = lmlist[4][1], lmlist[4][2]
+        x1, y1 = lmlist[4][1], lmlist[4][2]  #Point
         x2, y2 = lmlist[8][1], lmlist[8][2]
         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-
         
         length = math.hypot(x2 - x1, y2 - y1)
       
         cv2.circle(img, (x1,y1), 10, (255,0,255), cv2.FILLED)
         cv2.circle(img, (x2,y2), 10, (255,0,255), cv2.FILLED)
         cv2.line (img, (x1,y1),(x2,y2), (255,0,0),2 )
-        cv2.circle(img, (cx,cy),10, (255,0,255), cv2.FILLED)
+        
+        
 ########################################################################################################################            
       
-        # Normalisation de la valeur de length à la plage 0-255
+    #Normalisation de la valeur de length à la plage 0-255
         length = int(255 * (length - 40) / (1000 - 40))
         length = max(0, min(255, length))  # Assurer que la valeur est entre 0 et 255
+        
         print(length)
- # Calculer les composantes de couleur en fonction de la longueur
+    #Calculer les composantes de couleur en fonction de la longueur 
+   
     try: 
         color_red_to_blue = (int((1 - length / 255) * 255), 0, int((length / 255) * 255))
         color_blue_to_green = (0, int((1 - length / 255) * 255), int((length / 255) * 255))
     
-# Combinaison des deux couleurs pour obtenir une variation de couleur complète
-        color = (
-        color_red_to_blue[0] + color_blue_to_green[0],
+    #Combinaison des deux couleurs pour obtenir une variation de couleur complète
+        color = (                                                                                                                    #Cas qu'on ne peut pas gérer, on utilise try/except
+        color_red_to_blue[0] + color_blue_to_green[0],  
         color_red_to_blue[1] + color_blue_to_green[1],
         color_red_to_blue[2] + color_blue_to_green[2]
             )
-   
+        cv2.circle(img, (cx,cy),10, (color), cv2.FILLED)
         cv2.rectangle(img, (50, hCam - int(length)), (100, hCam), color, cv2.FILLED)
     except: 
             pass
+        
 ########################################################################################################################        
-  
+    #Calculer le FPS en temps réel
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
+    
+    
 ########################################################################################################################        
-   
+    #Inverser le sens de la vidéo
     flipped = cv2.flip(img, 2)
     
     # Affichage de la barre de graduation avec une couleur variable
     cv2.putText(flipped, f'{int(length)}', (50, hCam - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
     
-    cv2.putText(flipped, f'FPS: {int(fps)}', (40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,100),2) #Changement de taille d'écriture FPS
-    cv2.imshow("image", flipped)    #Inverser le sens de la vidéo
+    #Changement de taille d'écriture FPS
+    cv2.putText(flipped, f'FPS: {int(fps)}', (40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,100),2) 
     
-########################################################################################################################    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    
+    cv2.imshow("image", flipped)    
+    
+######################################################################################################################## 
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): #Pour Quitter la programmation test , appuyer sur "q"
         break
 cap.release()
 cv2.destroyAllWindows()
